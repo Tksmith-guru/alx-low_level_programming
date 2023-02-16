b98dbd8fab727916c745a381e2c14eaa972d98d8
Tksmith-guru <crunchyk837@gmail.com>
2023-02-16 11:10:48 -0800

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,17 @@
 #include<stdio.h>
+#include<unistd.h>
+
 /**
- * main - Entry point
- *
- * Return: Always 1 (Success)
+ * * main -Entry point
+ * *
+ * *Description: "and that piece of art that is useful"
+ * "-Dora Kopar, 2015-10-19"
+ * *
+ * *Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	putchar("and that piece of art is useful" "Dora Kopar, 2015-10-19\n");
+	write(1, "and that piece of art is useful\" - Dora Kopar, 2015-10-19\n", 59);
 	return (1);
 }
