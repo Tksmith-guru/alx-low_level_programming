81584ecc8a4b2c9cfc31b18628ec84c9088d970b
Tksmith-guru <crunchyk837@gmail.com>
2023-02-17 09:28:29 -0800

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char numbers;
+
+	for (number = '0'; number <= '9'; number++)
+	putchar(number);
+
+	for (number = 'a'; number <= 'f'; number++)
+	putchar(number);
+
+	putchar('\n');
+	return (0);
+}
