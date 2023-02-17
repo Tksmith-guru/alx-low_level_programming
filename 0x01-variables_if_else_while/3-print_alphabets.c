ed71722578c88e727f4235fa67a33330370ae15e
Tksmith-guru <crunchyk837@gmail.com>
2023-02-17 08:03:09 -0800

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabets;
+	char CAPS;
+
+	for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
+		putchar(alphabets);
+	for (CAPS = 'A' CAPS <= 'z'; CAPS++)
+		Putchar(CAPS);
+	Putchar('\n');
+	return (0);
+}
