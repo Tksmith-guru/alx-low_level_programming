7def7c91a1c57d7937966b064ae164bb7a58195c
Tksmith-guru <crunchyk837@gmail.com>
2023-02-17 11:03:20 -0800

    Task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Program that prints the possible combinations of two-digit numbers'
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{int first, second;
+for (first = 0; first < 100; first++)
+{
+for (second = 0; sceond < 100; second++)
+{
+if (first < second)
+{putchar((first / 10) + 48);
+putchar((first % 10) + 48);
+putchar(' ');
+putchar((second / 10) + 48);
+putchar((second % 10) + 48);
+if (first != 98 || second != 99)
+{putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
