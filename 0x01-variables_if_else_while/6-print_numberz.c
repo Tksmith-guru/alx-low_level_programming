ea486eced1c6d6b9c7d5d3670374a00b53257ee0
Tksmith-guru <crunchyk837@gmail.com>
2023-02-17 09:08:49 -0800

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int numbers;
+
+	for (number = 0; number < 10; number++)
+	putchar(number + '0');
+	putchar('\n');
+	return (0);
+}
