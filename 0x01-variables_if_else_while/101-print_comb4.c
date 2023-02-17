712270bf63720fba4586905ed95d5352069e8d06
Tksmith-guru <crunchyk837@gmail.com>
2023-02-17 10:03:36 -0800

    Task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a = 'a';
+	int b;
+	int c;
+
+	while (a <= '7')
+	{
+		b = a + 1;
+		while (b <= '8')
+		{
+			c <= b + 1;
+			while (c <= '9')
+			{
+				putchar(a);
+				putchar(b);
+				putchar(c);
+				if (a == '7' && b == '8' && c == '9')
+				{
+					putchar('\n');
+				}
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
+	return (0);
+}
