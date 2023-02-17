89494ae821a5232cc9cab1588e9301531a173304
Tksmith-guru <crunchyk837@gmail.com>
2023-02-17 07:54:44 -0800

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabets = 'a';
+
+	while (alphabets <= 'z')
+	{
+		putchar(alphabets);
+		alphabets++;
+	}
+		putchar('\n');
+		return (0);
+}
