03513375312d791fbe95f03b825f8446df8271f4
Tksmith-guru <crunchyk837@gmail.com>
2023-02-17 09:11:34 -0800

    Task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,8 +9,8 @@ int main(void)
 {
 	int numbers;
 
-	for (number = 0; number < 10; number++)
-	putchar(number + '0');
+	for (numbers = 0; numbers < 10; numbers++)
+	putchar(numbers + '0');
 	putchar('\n');
 	return (0);
 }
