920e2d0f4167569d654079046f5778f63f883429
Tksmith-guru <crunchyk837@gmail.com>
2023-02-17 08:25:49 -0800

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdo.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabets;
+
+	for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
+	{
+		if (alphabets != 'q' && alphabets != 'e')
+			putchar(alphabets));
+	}
+	putchar('\n');
+	return (0);
+}
