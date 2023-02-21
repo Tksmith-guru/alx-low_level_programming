80c9a8334803a404cc6a9f69f3742fdbc31316dd
Tksmith-guru <crunchyk837@gmail.com>
2023-02-21 14:37:36 -0800

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+
+	char i;
+
+
+	for (i = 'a'; i <= 'z'; i++)
+	
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
