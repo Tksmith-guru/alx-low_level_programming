be0dd462d2d6bbd0497810ced6e3bef88319133f
Tksmith-guru <crunchyk837@gmail.com>
2023-02-21 18:06:34 -0800

    Task 1mod

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,11 +2,9 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * print_alphabet - entry point
  *
- * Description: check the code
- *
- * Return:Always 0 (Success);
+ * Return: Always 0 (Success);
  */
 
 void print_alphabet(void)
