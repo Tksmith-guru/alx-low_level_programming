13e1e240d98c235a8a907ececd3fc028fc239371
Tksmith-guru <crunchyk837@gmail.com>
2023-02-21 16:28:37 -0800

    Task 0mod

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,12 @@
+#include <stdio.h>
 #include "main.h"
+
 /**
  * main - prints _putchar
  *
  * Return: always 0
  */
-int main(void);
-char newcharacters;
-
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
@@ -17,5 +17,6 @@ char newcharacters;
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
