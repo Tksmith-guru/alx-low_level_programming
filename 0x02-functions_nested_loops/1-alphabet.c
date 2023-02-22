a3c2920b5edcaf829ff92912a0da5afb53fa80b9
Tksmith-guru <crunchyk837@gmail.com>
2023-02-21 16:41:46 -0800

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+
+	char i;
+
+
+	for (i = 'a'; i <= 'z'; i++)
+	
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
