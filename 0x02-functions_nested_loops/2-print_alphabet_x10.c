eb85aae958a93acf617896797f9d05075d12d8fa
Tksmith-guru <crunchyk837@gmail.com>
2023-02-21 18:34:07 -0800

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * print_alphabet- Entry point
+ * print_alphabets- Entry point
  *
  * Return: Always 0 (Success);
  */
-void print_alphabet(void)
+void print_alphabets(void)
 {
 	char i;
 	char j;
