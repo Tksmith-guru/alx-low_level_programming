35f1bdb0365508a0f5dddf43a8f6bbdd610cf76f
Tksmith-guru <crunchyk837@gmail.com>
2023-02-22 09:30:59 -0800

    Task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * _islower - Entry point
+ * test_islower - Entry point
  *
  * @c: character to cmopare
  *
  * Return: 1 if true, 0 if false
  */
 
-int _islower(int c)
+int test_islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
 	{
