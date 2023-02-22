e59417bde37bcc65b2ecb3e225f772ff9788ae22
Tksmith-guru <crunchyk837@gmail.com>
2023-02-22 15:30:23 -0800

    Task 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,7 +14,7 @@ void print_remaining_days(int month, int day, int year)
 {
 if ((year % 4 == 0) || (year % 400 == 0 && year % 100 != 0))
 {
-if (month >= 3 && day >= 60)
+if (month >= 2 && day >= 60)
 {
 day++;
 }
@@ -30,7 +30,7 @@ printf("Invalid date: %02d/%02d/%04d\n", month day - 31, year);
 else
 {
 printf("Day of the year: %d\n", day);
-printf("Reamaining days: %d\n", 365 - day);
+printf("Remaining days: %d\n", 365 - day);
 }
 }
 }
