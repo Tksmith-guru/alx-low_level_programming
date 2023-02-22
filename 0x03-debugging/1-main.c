30dca151f8cdbaf3e0a5d973ee4fa703480f50b0
Tksmith-guru <crunchyk837@gmail.com>
2023-02-22 13:45:31 -0800

    Task 1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Causes an infinite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+int i;
+
+printf('Indefinite loop incoming :('\n');
+
+i = 0;
+/*
+ * while (i < 10)
+ * {
+ * putchar(i);
+ * }
+ */
+printf("Inndefinite loop avoided! \\o/\n");
+
+return (0);
+}
