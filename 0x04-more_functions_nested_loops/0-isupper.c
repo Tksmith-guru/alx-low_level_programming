857afc871ea815d4cba82012ee188afa1cfdd879
Tksmith-guru <crunchyk837@gmail.com>
2023-02-23 03:24:29 -0800

    mod Task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _isupper - checks if a character is uppercases or not
- * @c; character to be tested
+ * _isupper - checks if a character is uppercase or not
+ * @c: character to be tested
  * Return: 1 whether it is, 0 otherwise
  */
 
