f5aa9ed3e5941092d9fa307dab88cd16399b200c
Tksmith-guru <crunchyk837@gmail.com>
2023-04-04 13:23:59 -0700

    MOre fix task 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -26,7 +26,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		*head = new;
 		return (new);
 	}
-	currnet = *head;
+	current = *head;
 	while (current->next != NULL)
 	{
 		current = current->next;
