9500e283253728e0c08c050633dba968c93a44f6
Tksmith-guru <crunchyk837@gmail.com>
2023-04-04 17:06:42 -0700

    Task 14

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -10,6 +10,8 @@
 listint_t *find_listint_loop(listint_t *head)
 {
 	listint_t *trotoise, *hare;
+
+	tortoise = hare = head;
 	while (tortoise && hare && hare->next)
 	{
 		tortoise = tortoise->next;
