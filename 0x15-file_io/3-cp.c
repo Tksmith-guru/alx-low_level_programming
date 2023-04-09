6680b605429cafc9641ed77403178eb7277a4253
Tksmith-guru <crunchyk837@gmail.com>
2023-04-09 12:59:09 -0700

    MOre fixes

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -25,22 +25,22 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 	src = open(argv[1], O_RDONLY);
-	check_IO_stat(src, -1, argv[1], 'O');
+	check_IO_stat(src, -1, argv[1], 'o');
 	dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
-	check_IO_stat(dest, -1, argv[2], 'W');
+	check_IO_stat(dest, -1, argv[2], 'w');
 	while (n_read == 1024)
 	{
 		n_read = read(src, buffer, sizeof(buffer));
 		if (n_read == -1)
-			check_IO_stat(-1, -1, argv[1], 'O');
+			check_IO_stat(-1, -1, argv[1], 'o');
 		wrote = write(dest, buffer, n_read);
 		if (wrote == -1)
-			check_IO_stat(-1, -1, argv[2], 'W');
+			check_IO_stat(-1, -1, argv[2], 'w');
 	}
 	close_src = close(src);
-	check_IO_stat(close_src, src, NULL, 'C');
+	check_IO_stat(close_src, src, NULL, 'c');
 	close_dest = close(dest);
-	check_IO_stat(close_dest, dest, NULL, 'C');
+	check_IO_stat(close_dest, dest, NULL, 'c');
 	return (0);
 }
 
@@ -55,7 +55,7 @@ int main(int argc, char *argv[])
  */
 void check_IO_stat(int stat, int fd, char *filename, char mode);
 {
-	if (mode == 'C' && stat == -1)
+	if (mode == 'c' && stat == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
