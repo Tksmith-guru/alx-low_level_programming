5d2b84fbb4f7fedd61c3978f51b798b46f93256a
Tksmith-guru <crunchyk837@gmail.com>
2023-04-09 09:54:10 -0700

    Task 0

---
+++ 0x15-file_io/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _putchar - It writes the character c to stdout
+ * @c: the character to print
+ *
+ * Return: On success 1
+ * On error, -1 is returned and the error is set approximately
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
