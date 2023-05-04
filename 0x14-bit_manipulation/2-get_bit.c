0e41d353ae5c8bfe56ff57cb6eb5bac5bbb8d1cc
Tksmith-guru <crunchyk837@gmail.com>
2023-05-04 08:37:33 -0700

    task2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * get_index - It returns the value of a bit at a given index.
+ * get_bit - It returns the value of a bit at a given index.
  * @n: the number to be checked in terms of bit
  * @index: the index of the bit to be checked, starting from 0
  *
  * Return: the value of the bit at index
  * If an error occurs -1
  */
-int get_index(unsigned long int n, unsigned int index)
+int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int divide, confirm;
 
