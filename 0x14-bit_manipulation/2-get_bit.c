21f42aaee1456a9beb4397576e12405ba72caf07
Tksmith-guru <crunchyk837@gmail.com>
2023-05-04 08:29:58 -0700

    task 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * get_index - It returns the value of a bit at a given index.
+ * @n: the number to be checked in terms of bit
+ * @index: the index of the bit to be checked, starting from 0
+ *
+ * Return: the value of the bit at index
+ * If an error occurs -1
+ */
+int get_index(unsigned long int n, unsigned int index)
+{
+	unsigned long int divide, confirm;
+
+	if (index > (sizeof(unsigned long int) * 8 - 1))
+		return (-1);
+	divide = 1 << index;
+	confirm = n & divide;
+	if (confirm == divide)
+		return (1);
+	return (0);
+}
