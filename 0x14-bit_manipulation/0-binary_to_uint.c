f94d3cce21e09ae165600c5b2ea846814c0de20d
Tksmith-guru <crunchyk837@gmail.com>
2023-05-04 07:09:26 -0700

    MOre work task0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,29 +13,21 @@
 
 unsigned int binary_uint(const char *b)
 {
-	if (!b)
-	{
-		return (0);
-	}
-
-	int len = strlen(b);
+	int i, base = 1, len = 0;
 	unsigned int res = 0;
-	int i = len - 1;
-	int base = 1;
 
-	while (i >= 0)
+	if (!b)
+		return (0);
+
+	len = strlen(b);
+
+	for (i = len - 1; i >= 0; i--)
 	{
-		if (b[i] == '1')
-		{
-			res += base;
-		}
-		else if (b[i] != '0')
-		{
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		}
+		if (b[i] == '1')
+			res += base;
 		base *= 2;
-		i--;
 	}
-
 	return (res);
 }
